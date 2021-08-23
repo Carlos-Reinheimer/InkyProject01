b793740ba4b7e922ae43a00783d527ec638a6736
Carlos-Reinheimer <carlosgreinheimer@gmail.com>
2021-08-23 18:00:16 -0300

    Initial commit

---
+++ Assets/Inky/Story01.ink
@@ -0,0 +1,22 @@
+<i>Once upon a time...</i>
+
+How are you doing fellow player? #Carlos
+
+->My_Choises
+
+== My_Choises == 
+* [Fine] -> Good
+* [No good] -> Bad
+* [Nha] -> Other
+
+== Good ==
+<color=green>I am fine, thanks for asking.</color> #You
+-> END
+
+== Bad ==
+<color=red>Not so good, what about ya? </color> #You
+-> My_Choises
+
+== Other ==
+<color=yellow>Can you repeat please? </color> #You
+-> My_Choises
---
+++ Assets/Scripts/InkyScriot.cs
@@ -0,0 +1,69 @@
+using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InkyScriot : MonoBehaviour {
+
+    public TextAsset inkJson;
+    public Text textPrefab;
+    public Button buttonPrefab; 
+
+    private Story story;
+    void Start() {
+        story = new Story(inkJson.text);
+
+        refreshUI();
+    }
+
+    void refreshUI() {
+
+        eraseUI();
+
+        Text storyText = Instantiate(textPrefab) as Text;
+        string text = loadStoryChunk();
+
+        List<string> tags = story.currentTags;
+        if (tags.Count > 0) text = "<b>" + tags[0] + "</b>" + " - " + text;
+
+        storyText.text = text;
+        storyText.transform.SetParent(this.transform, false);
+
+        foreach (Choice choice in story.currentChoices) {
+            Button choiceButton = Instantiate(buttonPrefab) as Button;
+            Text choiceText = buttonPrefab.GetComponentInChildren<Text>();
+            choiceText.text = choice.text;
+            choiceButton.transform.SetParent(this.transform, false);
+
+            choiceButton.onClick.AddListener(delegate { // send a method as a parameter for another method
+                chooseStoryChoice(choice);
+            });
+        }
+    }
+    void eraseUI() {
+        for (int i = 0; i < this.transform.childCount; i++) {
+            Destroy(this.transform.GetChild(i).gameObject);
+        }
+    }
+
+    void chooseStoryChoice(Choice choice) {
+        story.ChooseChoiceIndex(choice.index);
+
+        refreshUI();
+    }
+
+
+    string loadStoryChunk() { // returns a string
+        // ------------ STORY API --------------
+
+        //story.Continue(); // will load the next line
+
+        string text = "";
+        if (story.canContinue) {
+            text = story.ContinueMaximally(); // will load until hit choises or end story
+        }
+
+        return text;
+    }
+}
